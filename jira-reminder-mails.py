3a9c12a42a2fb271eb172ea8ee161ca8d9d40175
Tobias Bartel <tobias@bartel.ws>
2013-07-17 09:41:11 +0200

    Update jira-reminder-mails.py
    typo

--- jira-reminder-mails.py
+++ jira-reminder-mails.py
@@ -15,8 +15,7 @@ j = JIRA(options, basic_auth=('username', 'password'))
 result = j.search_issues('project = "My Project" AND status = resolved AND resolved < -4d')
 
 for issue in result:
-    msg = MIMEText("Hello %s,\n\nyou are receiving this EMail because there is an issue at %s/browse/%s that needs to be reviewed by you.\nPlease go to %s/browse/%s and either close
-or reopen \"%s - %s\".\n\nKind regards,\n\nThe Team"  % (issue.fields.reporter.displayName, options['server'], issue.key, options['server'], issue.key, issue.key, issue.fields.summary))
+    msg = MIMEText("Hello %s,\n\nyou are receiving this EMail because there is an issue at %s/browse/%s that needs to be reviewed by you.\nPlease go to %s/browse/%s and either close or reopen \"%s - %s\".\n\nKind regards,\n\nThe Team"  % (issue.fields.reporter.displayName, options['server'], issue.key, options['server'], issue.key, issue.key, issue.fields.summary))
     msg['Subject'] = 'Please review "%s" - %s ' % (issue.fields.summary, issue.key)
     msg['From'] = 'Web Operations Team <foo@bar.com>'
     msg['To'] = '%s <%s>' % (issue.fields.reporter.displayName, issue.fields.reporter.emailAddress)
